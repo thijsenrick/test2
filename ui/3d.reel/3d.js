c62bf0ee6d5e0ca0ebe1c20c948c0a727b62e207
thijsenrick <noreply>
2015-10-27 13:32:59 +0000

    Add component 3d

---
+++ ui/3d.reel/3d.js
@@ -0,0 +1,16 @@
+/**
+ * @module ui/3d.reel
+ */
+var Component = require("montage/ui/component").Component;
+
+/**
+ * @class 3d
+ * @extends Component
+ */
+exports.3d = Component.specialize(/** @lends 3d# */ {
+    constructor: {
+        value: function 3d() {
+            this.super();
+        }
+    }
+});
